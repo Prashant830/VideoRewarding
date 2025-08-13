5d435b5d6d40222049f946408f1be59cd8e9a29d
Prashant Singh <prashantsingh@Prashants-Laptop.local>
2025-08-13 13:44:08 +0530

    updated

--- Backend/src/models/videoModel.js
+++ Backend/src/models/videoModel.js
@@ -6,12 +6,8 @@ async function getAllVideos() {
                          "https://youtube.com/shorts/dTanksRR8Ac?si=d-F11cekwwoBxgqR" ,
                          "https://youtube.com/shorts/lYktrLTfIY4?si=t5Lpw7ctTxlDCUkc" ,
                          "https://youtube.com/shorts/1vh0tt2lKy8?si=84uL40e9OPuWqIj7" ,
-                         "https://youtube.com/shorts/XAidJjuh5gk?si=fZPlWxrCTmtRnzg_" ,
-                         "https://youtube.com/shorts/8453RdTR_hk?si=yR-BnDKFDC74iRFz" ,
-                         "https://youtube.com/shorts/DmVCSeWbl8I?si=2eRCKOe5p_td7lA0" ,
-                         "https://youtube.com/shorts/VpOyHbGV7sw?si=W3i0WNmWe3P2_-lB" ,
-                         "https://youtube.com/shorts/aXK3hAYxwPA?si=NBFN9Uu5SA4gEHqH" ,
-                         "https://youtube.com/shorts/88QAcMEFYes?si=xQKehkl529Dp2-SS" ]
+                         "https://youtube.com/shorts/XAidJjuh5gk?si=fZPlWxrCTmtRnzg_"
+                        ]
         return videos;
     } catch (err) {
         console.error('Error in token generation:', err.message); 
--- Backend/src/services/videoService.js
+++ Backend/src/services/videoService.js
@@ -3,14 +3,23 @@ const AppError = require('../utils/AppError');
 
 async function getVideos() {
     try {
-        const videos = await videoModel.getAllVideos();
-        if (!videos) throw new AppError('Videos not found', 401);
+        const videoUrls = await videoModel.getAllVideos(); // direct call for now, or videoModel.getAllVideos();
+        if (!videoUrls || videoUrls.length === 0) {
+            throw new AppError('Videos not found', 401);
+        }
+
+        // Convert to desired schema
+        const videos = videoUrls.map((url, index) => ({
+            videoId: index + 1,
+            videoUrl: url,
+            currentWatched: 0
+        }));
+
         return videos;
     } catch (error) {
         throw new AppError(error.message, 401);
     }
 }
-
 
 
 module.exports = { getVideos};
