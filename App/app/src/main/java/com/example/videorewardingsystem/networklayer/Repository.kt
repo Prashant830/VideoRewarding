04509f547f54d7348a7a62a17675a44a8c451c9a
Prashant Singh <prashantsingh@Prashants-Laptop.local>
2025-08-14 17:32:55 +0530

    added error handlling

--- App/app/src/main/java/com/example/videorewardingsystem/networklayer/Repository.kt
+++ App/app/src/main/java/com/example/videorewardingsystem/networklayer/Repository.kt
@@ -3,15 +3,25 @@ package com.example.videorewardingsystem.networklayer
 import com.example.videorewardingsystem.networklayer.retrofit.ApiService
 import com.example.videorewardingsystem.networklayer.retrofit.model.RewardModel
 import com.example.videorewardingsystem.networklayer.retrofit.model.VideoResponse
+import okhttp3.ResponseBody
 import retrofit2.Response
+
 
 class Repository(private val apiService: ApiService) : ApiService {
 
-    override suspend fun getVideos(): Response<VideoResponse> {
-        return apiService.getVideos()
-    }
+  override suspend fun getVideos(): Response<VideoResponse> {
+            return try {
+                apiService.getVideos()
+            } catch (e: Exception) {
+                Response.error(500, ResponseBody.create(null, e.message.toString()))
+            }
+        }
 
-    override suspend fun sendReward(request: RewardModel): Response<Any> {
-        return apiService.sendReward(request)
-    }
+        override suspend fun sendReward(request: RewardModel): Response<Any> {
+            return try {
+                apiService.sendReward(request)
+            } catch (e: Exception) {
+                Response.error(500, ResponseBody.create(null, e.message.toString()))
+            }
+        }
 }
